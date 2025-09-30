2d82f58800c0863ace21ea3878c4836e9cd84254
GeonRyoung <rjsfud7467@naver.com>
2025-09-30 22:26:44 +0900

    Initial commit

---
+++ src/test/java/com/example/tmdb_project/TmdbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmdb_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
